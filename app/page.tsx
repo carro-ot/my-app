ad76f6999c3d3869c9c10ab333a25aaee6fe23d0
carro-ot <168121508+carro-ot@users.noreply.github.com>
2025-07-12 23:44:31 +0300

    time 23:44

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
-import { TopBar } from "@/components/shared";
-
+import { Container, Filters, TopBar } from "@/components/shared";
+import { Title } from "@/components/shared/title";
 
 export default function Home(){
 	return (
 		<>
+			<Container className="mt-10">
+				<Title text="Ассортимент" size="lg" className="font-extrabold" />
+			</Container>
 			<TopBar />
+			
+			<Container className="[ mt-10 pb-14]">
+				<div className="flex gap-[60px]">
 
+				<div className="w-{250px}">
+						<Filters/>
+					</div>
+
+					
+			</div>
+			</Container>
 		</>
 	);
 }
--- components/shared/top-bar.tsx
+++ components/shared/top-bar.tsx
@@ -1,9 +1,8 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
-import { Container } from './container';
-import { Title } from './title';
 import { Categories } from './categories';
 import { SortPopup } from './sort-popup';
+import { Container } from './container';
 
 interface Props {
 	className?: string;
@@ -12,8 +11,7 @@ interface Props {
 export const TopBar: React.FC<Props> = ({ className }) => {
   return (
 	  <div className={cn('sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10', className)}>
-		  <Container className="mt-10">
-				<Title text="Ассортимент" size="lg" className="font-extrabold" />
+		 <Container className="mt-10">
 				<Categories />
 				<SortPopup/>
 			</Container>
